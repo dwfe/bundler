d2c2e4b09f43e45a97526af5f5419132274733c6
z <alex@dwfe.ru>
2022-01-30 20:30:55 +0300

    .

--- src/util/params.ts
+++ src/util/params.ts
@@ -13,11 +13,16 @@ export const OVERRIDE_CONFIG_FILE = relativeToBase('webpack.config.js');
 //endregion
 
 
-//region Objects
+//region Objects/Modules
+
+const optionsMapFieldName = 'dwfeBundlerOptions';
 
 export const PKG = require(PKG_FILE);
-export const OPTIONS_MAP = PKG.dwfeBundlerOptions;
+export const OPTIONS_MAP = PKG[optionsMapFieldName];
 export const OVERRIDE_CONFIG = existsSync(OVERRIDE_CONFIG_FILE) ? require(OVERRIDE_CONFIG_FILE) : null;
+
+if (!OPTIONS_MAP || !Object.keys(OPTIONS_MAP).length)
+  throw new Error(`To run the bundler, specify an object with options in field "${optionsMapFieldName}" of the file package.json`);
 
 //endregion
 
